cb9fced7646cdd1070ea5562052262025566a39b
Habeeb Lateef <77229868+devhabeeblateef@users.noreply.github.com>
2024-02-17 18:21:07 +0100

    change gitignore

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Hero from "./hero";
+import Show from "./show";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <Hero/>
+      <Show/>
     </>
       );
 }
