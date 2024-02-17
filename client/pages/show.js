00e15ad4a9e54b9cf820ed73f4564fb45dcd39a9
Habeeb Lateef <77229868+devhabeeblateef@users.noreply.github.com>
2024-02-17 23:18:18 +0100

    fix size issues

--- client/pages/show.js
+++ client/pages/show.js
@@ -3,13 +3,13 @@ import Image from 'next/image'
 
 function Show() {
   return (
-    <div className='h-screen text-[#FFFFF0] w-screen bg-[#0C0C0C]'>
+    <div className='h-full text-[#FFFFF0] w-screen bg-[#0C0C0C]'>
       <p className='text-4xl text-center m-auto pt-16'>IB Concepts</p>
       <p className='text-center pt-2 text-md'>Creativity at its peak.</p>
       <p className='text-blue-500 m-auto mt-2 text-center'>Learn more &gt; </p>
 
       <div>
-        <Image src="/building.png" width={300} height={300} className='pt-14 p-0 m-auto' alt="IB Concepts" />
+        <Image src="/building.png" width={300} height={300} className='pt-12 p-0 m-auto' alt="IB Concepts" />
       </div>
     </div>
   )
