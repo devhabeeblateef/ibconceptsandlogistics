5dab587328e326e5e70b78128b11a100e2264df1
Habeeb Lateef <77229868+devhabeeblateef@users.noreply.github.com>
2024-02-17 17:26:11 +0100

    add react,bgimage

---
+++ client/pages/hero.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div>
+    <div className='bg-bgimage absolute z-0 text-white w-screen h-screen bg-cover bg-center bg-no-repeat filter brightness-50'>
+        
+    </div>
+    <div className='text-white absolute z-10'>
+    Hello
+    </div>
+    </div>
+  )
+}
+
+export default Hero
